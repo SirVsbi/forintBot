1c2bee64a6651cb579d16fdbf468cd59cdcf7e7d
SirVsbi <sirmrvsbi@gmail.com>
2022-07-11 16:21:04 +0300

    add more data

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 import json
 
+from discord.ext.commands import bot
 from google_currency import convert
 import os
 import json
 import datetime
 import discord
 from dotenv import load_dotenv
+from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+client = commands.Bot(command_prefix='/')
 
 
 def getEUR_HUF_exhange_rate():
     exchange_rate = convert('eur', 'huf', 1)
     rate_json = json.loads(exchange_rate)
     save_exchange_rate(rate_json["amount"])
+    return rate_json["amount"]
+
+
+def get_any_to_any_exchange_rate(currency_from: str, currency_to: str, amount: int) -> float:
+    exchange_rate = convert(currency_from, currency_to, amount)
+    rate_json = json.loads(exchange_rate)
     return rate_json["amount"]
 
 
@@ -41,17 +49,23 @@ def save_exchange_rate(rate):
                   separators=(',', ': '))
 
 
-def generate_embed():
+def generate_embed(currency_from: str, currency_to: str, exchange_rate, text: str | None = None) -> discord.Embed:
     embed = discord.Embed(
         title="Doktorminiszterelnökúr, a mostani jelentés készen van",
         colour=discord.Color.from_rgb(46, 26, 71)  # AB7FBC
     )
     embed.set_thumbnail(
         url="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Parliament_Building%2C_Budapest%2C_outside.jpg/1200px-Parliament_Building%2C_Budapest%2C_outside.jpg")
-    embed.add_field(
-        name="EUR <-> HUF",
-        value=getEUR_HUF_exhange_rate()
-    )
+    if text is None:
+        embed.add_field(
+            name=currency_from + "<->" + currency_to,
+            value=exchange_rate
+        )
+    else:
+        embed.add_field(
+            name=text,
+            value="Nem létezik ilyen valuta"
+        )
     return embed
 
 
@@ -69,13 +83,34 @@ async def on_ready():
     print(f'Guild Members:\n - {members}')
     channel = client.get_channel(990292982274097302)
     print(channel)
-    await channel.send \
-        (embed=generate_embed())
+
+
+#   await channel.send(embed=generate_embed())
 
 
 @client.event
 async def send_message(message):
     pass
+
+
+@client.command()
+async def árfolyam(ctx, *args):
+    if len(args) == 0:
+        exchange_rate = getEUR_HUF_exhange_rate()
+        embed = generate_embed("EUR", "HUF", exchange_rate)
+    elif len(args) == 1:
+        embed = exchange_any_to_huf(args[0])
+    elif len(args):
+        embed = exchange_any_to_any(args[0], args[1])
+    channel = client.get_channel(990292982274097302)
+    await ctx.channel.send(embed=embed)
+
+
+def exchange_any_to_any(currency_from = "eur", currency_to = "huf", amount = 1) -> discord.Embed:
+    exchange_rate = get_any_to_any_exchange_rate(currency_from, currency_to, amount)
+    if exchange_rate == 0:
+        return generate_embed(currency_from, "HUF", 0, "Hibás valuta")
+    return generate_embed(currency_from, "HUF", exchange_rate)
 
 
 client.run(TOKEN)
